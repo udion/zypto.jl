bc8f339b3a071b6f670119cec165b71dfca1a27f
udion <uddeshya.upa@gmail.com>
2017-07-03 12:14:58 +0800

    exported the sbox2string and string2sbox

--- src/zypto.jl
+++ src/zypto.jl
@@ -1,5 +1,5 @@
 module zypto
   include("sbox_utilities.jl")
   export int2bool, bool2int, all_bool, vec_dot, vec_xor, walsh_trans, cross_corr, auto_corr, non_linearity
-  export transformSbox, ddt, sbox_nonlinearity
+  export transformSbox, ddt, sbox_nonlinearity, sbox2string, string2sbox
 end
